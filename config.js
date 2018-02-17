a567569ea9433100d7afa54f406ea67ff5fcc584
vitpavlenko <vitaliypavlenko01@gamil.com>
2018-02-17 23:24:37 +0200

    server for my labkas

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: 'mongodb://vitaliypavlenko:c1999500453@ds223268.mlab.com:23268/familybudget',
-    port: 3000,
+    port: 80,
     secret: 'act badly fed exact',
     hash: 'c1999500453'
 }
